5ed64f239c26b2f31142c48aa897787d62cf01ac
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-11-03 12:06:58 +0100

    adjusting sql query lengths

--- DCS_STATUS/get_gem_dcs_status.sh
+++ DCS_STATUS/get_gem_dcs_status.sh
@@ -4,7 +4,7 @@ echo "Start time $(date -u)"
 cp template.sql request.sql
 sed -i -e "s/CURRENT_DATE/$(date -u +'%Y.%m.%d %H:%M:%S')/g" request.sql
 sed -i -e "s/START_DATE_SHORT/$(date -d '-2 day' -u +'%Y.%m.%d %H:%M:%S')/g" request.sql
-sed -i -e "s/START_DATE_LONG/$(date -d '-7 day' -u +'%Y.%m.%d %H:%M:%S')/g" request.sql
+sed -i -e "s/START_DATE_LONG/$(date -d '-30 day' -u +'%Y.%m.%d %H:%M:%S')/g" request.sql
 
 sqlplus CMS_COND_GENERAL_R/p3105rof@cms_omds_adg	@request.sql > /dev/null
 
